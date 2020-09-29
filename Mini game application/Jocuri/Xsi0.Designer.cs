fb0ced82820a797a1a472cee0043613a9d4bfc81
Gabriel Olariu <72080772+gabriel-olariu@users.noreply.github.com>
2020-09-29 15:50:44 +0300

    Add files via upload

---
+++ Mini game application/Jocuri/Xsi0.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Jocuri
+{
+    partial class Xsi0
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Xsi0));
+            this.Remiza = new System.Windows.Forms.Label();
+            this.OWins = new System.Windows.Forms.Label();
+            this.XWins = new System.Windows.Forms.Label();
+            this.EButton = new System.Windows.Forms.Button();
+            this.RButton = new System.Windows.Forms.Button();
+            this.NGButton = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A00 = new System.Windows.Forms.Button();
+            this.CreditLink = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Remiza
+            // 
+            this.Remiza.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remiza.Location = new System.Drawing.Point(294, 222);
+            this.Remiza.Name = "Remiza";
+            this.Remiza.Size = new System.Drawing.Size(100, 23);
+            this.Remiza.TabIndex = 44;
+            this.Remiza.Text = "label3";
+            // 
+            // OWins
+            // 
+            this.OWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OWins.Location = new System.Drawing.Point(294, 134);
+            this.OWins.Name = "OWins";
+            this.OWins.Size = new System.Drawing.Size(100, 23);
+            this.OWins.TabIndex = 43;
+            this.OWins.Text = "label2";
+            // 
+            // XWins
+            // 
+            this.XWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XWins.Location = new System.Drawing.Point(294, 42);
+            this.XWins.Name = "XWins";
+            this.XWins.Size = new System.Drawing.Size(100, 23);
+            this.XWins.TabIndex = 42;
+            this.XWins.Text = "label1";
+            // 
+            // EButton
+            // 
+            this.EButton.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.EButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EButton.ForeColor = System.Drawing.Color.Tomato;
+            this.EButton.Location = new System.Drawing.Point(252, 319);
+            this.EButton.Name = "EButton";
+            this.EButton.Size = new System.Drawing.Size(85, 70);
+            this.EButton.TabIndex = 41;
+            this.EButton.Text = "Iesire";
+            this.EButton.UseVisualStyleBackColor = false;
+            this.EButton.Click += new System.EventHandler(this.EButton_Click);
+            // 
+            // RButton
+            // 
+            this.RButton.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.RButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RButton.ForeColor = System.Drawing.Color.Tomato;
+            this.RButton.Location = new System.Drawing.Point(114, 319);
+            this.RButton.Name = "RButton";
+            this.RButton.Size = new System.Drawing.Size(110, 70);
+            this.RButton.TabIndex = 40;
+            this.RButton.Text = "Resetare";
+            this.RButton.UseVisualStyleBackColor = false;
+            this.RButton.Click += new System.EventHandler(this.RButton_Click);
+            // 
+            // NGButton
+            // 
+            this.NGButton.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.NGButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NGButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NGButton.ForeColor = System.Drawing.Color.Tomato;
+            this.NGButton.Location = new System.Drawing.Point(5, 319);
+            this.NGButton.Name = "NGButton";
+            this.NGButton.Size = new System.Drawing.Size(85, 70);
+            this.NGButton.TabIndex = 39;
+            this.NGButton.Text = "Joc nou";
+            this.NGButton.UseVisualStyleBackColor = false;
+            this.NGButton.Click += new System.EventHandler(this.NGButton_Click);
+            // 
+            // A22
+            // 
+            this.A22.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A22.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A22.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A22.ForeColor = System.Drawing.Color.Tomato;
+            this.A22.Location = new System.Drawing.Point(187, 187);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(85, 82);
+            this.A22.TabIndex = 38;
+            this.A22.UseVisualStyleBackColor = false;
+            this.A22.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A21
+            // 
+            this.A21.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A21.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A21.ForeColor = System.Drawing.Color.Tomato;
+            this.A21.Location = new System.Drawing.Point(96, 187);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(85, 82);
+            this.A21.TabIndex = 37;
+            this.A21.UseVisualStyleBackColor = false;
+            this.A21.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A20
+            // 
+            this.A20.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A20.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A20.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A20.ForeColor = System.Drawing.Color.Tomato;
+            this.A20.Location = new System.Drawing.Point(5, 187);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(85, 82);
+            this.A20.TabIndex = 36;
+            this.A20.UseVisualStyleBackColor = false;
+            this.A20.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A02
+            // 
+            this.A02.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A02.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A02.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A02.ForeColor = System.Drawing.Color.Tomato;
+            this.A02.Location = new System.Drawing.Point(187, 11);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(85, 82);
+            this.A02.TabIndex = 35;
+            this.A02.UseVisualStyleBackColor = false;
+            this.A02.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A12
+            // 
+            this.A12.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A12.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A12.ForeColor = System.Drawing.Color.Tomato;
+            this.A12.Location = new System.Drawing.Point(187, 99);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(85, 82);
+            this.A12.TabIndex = 34;
+            this.A12.UseVisualStyleBackColor = false;
+            this.A12.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A11
+            // 
+            this.A11.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A11.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A11.ForeColor = System.Drawing.Color.Tomato;
+            this.A11.Location = new System.Drawing.Point(96, 99);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(85, 82);
+            this.A11.TabIndex = 33;
+            this.A11.UseVisualStyleBackColor = false;
+            this.A11.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A01
+            // 
+            this.A01.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A01.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A01.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A01.ForeColor = System.Drawing.Color.Tomato;
+            this.A01.Location = new System.Drawing.Point(96, 11);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(85, 82);
+            this.A01.TabIndex = 32;
+            this.A01.UseVisualStyleBackColor = false;
+            this.A01.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A10
+            // 
+            this.A10.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A10.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A10.ForeColor = System.Drawing.Color.Tomato;
+            this.A10.Location = new System.Drawing.Point(5, 99);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(85, 82);
+            this.A10.TabIndex = 31;
+            this.A10.UseVisualStyleBackColor = false;
+            this.A10.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A00
+            // 
+            this.A00.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.A00.Cursor = System.Windows.Forms.Cursors.Default;
+            this.A00.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A00.ForeColor = System.Drawing.Color.Tomato;
+            this.A00.Location = new System.Drawing.Point(5, 11);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(85, 82);
+            this.A00.TabIndex = 30;
+            this.A00.UseVisualStyleBackColor = false;
+            this.A00.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // CreditLink
+            // 
+            this.CreditLink.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreditLink.Location = new System.Drawing.Point(411, 394);
+            this.CreditLink.Name = "CreditLink";
+            this.CreditLink.Size = new System.Drawing.Size(100, 23);
+            this.CreditLink.TabIndex = 45;
+            this.CreditLink.TabStop = true;
+            this.CreditLink.Text = "© Olariu Gabriel";
+            this.CreditLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.CreditLink_LinkClicked);
+            // 
+            // Xsi0
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 406);
+            this.Controls.Add(this.CreditLink);
+            this.Controls.Add(this.Remiza);
+            this.Controls.Add(this.OWins);
+            this.Controls.Add(this.XWins);
+            this.Controls.Add(this.EButton);
+            this.Controls.Add(this.RButton);
+            this.Controls.Add(this.NGButton);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A00);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Xsi0";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Xsi0_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Remiza;
+        private System.Windows.Forms.Label OWins;
+        private System.Windows.Forms.Label XWins;
+        private System.Windows.Forms.Button EButton;
+        private System.Windows.Forms.Button RButton;
+        private System.Windows.Forms.Button NGButton;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.LinkLabel CreditLink;
+    }
+}
---
+++ Mini game application/Jocuri/Xsi0.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jocuri
+{
+    public partial class Xsi0 : Form
+    {
+        public Xsi0()
+        {
+            //ACEST PROIECT ESTE REALIZAT DE CATRE OLARIU GABRIEL
+
+            InitializeComponent();
+        }
+        public int player = 2; //par = Tura lui X; impar = Tura lui O
+        public int turns = 0; //numara turele
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0;//numara meciurile castigate
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "")
+            {
+
+                if (player % 2 == 0)
+                {
+                    button.Text = "X";
+                    player++;
+                    turns++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    player++;
+                    turns++;
+                }
+
+                if (CheckDraw() == true)
+                {
+                    MessageBox.Show("Remiza!!");
+                    sd++;
+                    NewGame();
+                }
+
+                if (CheckWinner() == true)
+                {
+                    if (button.Text == "X")
+                    {
+                        MessageBox.Show("X A castigat!!");
+                        s1++;
+                        NewGame();
+                        player = 2;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("O A castigat!!");
+                        s2++;
+                        NewGame();
+                        player = 1;
+                    }
+
+                }
+            }
+        }
+
+        private void EButton_Click(object sender, EventArgs e)//buton de iesire ce te redirectioneaza catre meniu
+        {
+            this.Hide();
+            Meniu Form2 = new Meniu();
+            Form2.ShowDialog();
+            this.Close();
+        }
+
+        private void Xsi0_Load(object sender, EventArgs e)
+        {
+            XWins.Text = "X: " + s1;
+            OWins.Text = "O: " + s2;
+            Remiza.Text = "Remize: " + sd;
+        }
+        void NewGame()
+        {
+            if (player % 2 == 0)//in mod normal daca variabila player e para ar trebui sa inceapa X-ul, dar incepe O-ul si nu stiu de ce...
+            {    MessageBox.Show("O incepe");
+                player++;
+            }
+            else
+            {
+                MessageBox.Show("X incepe");
+                player++;
+            }
+            turns = 0;
+            A00.Text = A01.Text = A02.Text = A10.Text = A11.Text = A12.Text = A20.Text = A21.Text = A22.Text = "";
+            XWins.Text = "X: " + s1;
+            OWins.Text = "O: " + s2;
+            Remiza.Text = "Remize: " + sd;
+        }
+
+        private void NGButton_Click(object sender, EventArgs e)//incepe joc nou
+        {
+            NewGame();
+        }
+        bool CheckDraw()
+        {
+            if ((turns == 9) && CheckWinner() == false)
+                return true;
+            else
+                return false;
+        }
+        bool CheckWinner()
+        {
+            //verifica pe orizontala
+            if ((A00.Text == A01.Text) && (A01.Text == A02.Text) && A00.Text != "")
+                return true;
+            else if ((A10.Text == A11.Text) && (A11.Text == A12.Text) && A10.Text != "")
+                return true;
+            else if ((A20.Text == A21.Text) && (A21.Text == A22.Text) && A20.Text != "")
+                return true;
+
+
+            //verifica pe verticala
+            if ((A00.Text == A10.Text) && (A10.Text == A20.Text) && A00.Text != "")
+                return true;
+            else if ((A01.Text == A11.Text) && (A11.Text == A21.Text) && A01.Text != "")
+                return true;
+            else if ((A02.Text == A12.Text) && (A12.Text == A22.Text) && A02.Text != "")
+                return true;
+
+            //verifica pe diagonala
+            if ((A00.Text == A11.Text) && (A11.Text == A22.Text) && A00.Text != "")
+                return true;
+            else if ((A02.Text == A11.Text) && (A11.Text == A20.Text) && A02.Text != "")
+                return true;
+            else
+                return false;
+
+        }
+
+        private void RButton_Click(object sender, EventArgs e)//buton ce reseteaza progresul jocului
+        {
+            s1 = s2 = sd = 0;
+            NewGame();
+        }
+
+        private void CreditLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)//un link pentru credits
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/olariu.gabriel.98");
+
+        }
+    }
+}
